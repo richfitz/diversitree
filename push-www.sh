e1ae37c80cf9c28494839b108c2867bb9c8e2af8
Rich FitzJohn <fitzjohn@zoology.ubc.ca>
2012-03-23 08:05:53 -0700

    Simplify push-www

--- push-www.sh
+++ push-www.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-cp -p logo/logo.* logo/beetles.eps www
-cd www/bib; make all install
-cd -
-rsync --delete --exclude .hg -e ssh -rvzL www/ zoology.ubc.ca:www/diversitree
+## cp -p logo/logo.* logo/beetles.eps www
+make -C www/bib all install
+rsync --delete -e ssh -rvzL www/ zoology.ubc.ca:www/diversitree
