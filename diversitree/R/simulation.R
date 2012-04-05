dba93f9c80d1037ca96f492ab78a196f763b2360
Emma Goldberg <eeg@uic.edu>
2012-04-04 19:25:57 -0600

    GeoSSE simulator works.
    It's actually just a wrapper function for the ClaSSE simulator.  But it
    works as one would expect, with geosse parameter names and states, via
    tree.geosse() or trees(type="geosse").
    
    The helper function pars.ge.to.cl() converts geosse pars to classe.pars,
    but it is not exposed.

--- diversitree/R/simulation.R
+++ diversitree/R/simulation.R
@@ -16,8 +16,8 @@
 ## 'model' object, I will design the calling structure in a way that
 ## is similar to S3 generics/methods.
 trees <- function(pars,
-                  type=c("bisse", "bisseness", "bd", "classe", "musse",
-                    "quasse", "yule"), n=1,
+                  type=c("bisse", "bisseness", "bd", "classe", "geosse",
+                         "musse", "quasse", "yule"), n=1,
                   max.taxa=Inf, max.t=Inf, include.extinct=FALSE,
                   ...) {
   if ( is.infinite(max.taxa) && is.infinite(max.t) )
@@ -28,6 +28,7 @@ trees <- function(pars,
               bisseness=tree.bisseness,
               bd=tree.bd,
               classe=tree.classe,
+              geosse=tree.geosse,
               musse=tree.musse,
               yule=tree.yule)
   trees <- vector("list", n)  
