4d1b76cb4b8104bc972ba01a10f7e3628da28784
Rich FitzJohn <fitzjohn@zoology.ubc.ca>
2012-04-17 15:38:53 -0700

    Avoid confusion about which bib file to add to by deleting the copied one

--- push-www.sh
+++ push-www.sh
@@ -2,3 +2,5 @@
 ## cp -p logo/logo.* logo/beetles.eps www
 make -C www/bib all install
 rsync -aLvz -e ssh --delete www/ zoology.ubc.ca:www/diversitree
+## Avoid future confusion about which file to add into!
+rm -f www/studies.bib
