38ee5321957668209ebc44d96d39708ca5083cdb
Rich FitzJohn <fitzjohn@zoology.ubc.ca>
2012-05-02 20:25:24 -0700

    Update bm direct->pruning

--- diversitree-tests/runit-bm.R
+++ diversitree-tests/runit-bm.R
@@ -12,14 +12,14 @@ test.bm <- function() {
   fit1 <- find.mle(make.bm(bird.orders, x), .1,
                    control=list(method="vcv"))
 
-  ## With the direct calculations
-  lik.direct <- make.bm(bird.orders, x, control=list(method="direct"))
-  fit2 <- find.mle(lik.direct, .1)
+  ## With the pruning calculations
+  lik.pruning <- make.bm(bird.orders, x, control=list(method="pruning"))
+  fit2 <- find.mle(lik.pruning, .1)
 
-  ## With the C-based direct calculations
-  lik.direct.C <- make.bm(bird.orders, x,
-                          control=list(method="direct", backend="C"))
-  fit3 <- find.mle(lik.direct.C, .1)
+  ## With the C-based pruning calculations
+  lik.pruning.C <- make.bm(bird.orders, x,
+                           control=list(method="pruning", backend="C"))
+  fit3 <- find.mle(lik.pruning.C, .1)
 
   all.equal(fit2, fit3)
   
@@ -45,12 +45,12 @@ test.ou <- function() {
   lik.vcv <- make.ou(bird.orders, x, control=list(method="vcv"))
   system.time(fit1 <- find.mle(lik.vcv, c(.1, .1, .1)))
 
-  lik.direct <- make.ou(bird.orders, x, control=list(method="direct"))
-  system.time(fit2 <- find.mle(lik.direct, c(.1, .1, .1)))
+  lik.pruning <- make.ou(bird.orders, x, control=list(method="pruning"))
+  system.time(fit2 <- find.mle(lik.pruning, c(.1, .1, .1)))
 
-  lik.direct.C <- make.ou(bird.orders, x,
-                          control=list(method="direct", backend="C"))
-  system.time(fit3 <- find.mle(lik.direct.C, c(.1, .1, .1)))
+  lik.pruning.C <- make.ou(bird.orders, x,
+                           control=list(method="pruning", backend="C"))
+  system.time(fit3 <- find.mle(lik.pruning.C, c(.1, .1, .1)))
 
   all.equal(fit1, fit2)
   all.equal(fit1, fit3)
