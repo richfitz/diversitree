25646b71a216f83ad2389d423dc7dbf64113f5a1
Rich FitzJohn <fitzjohn@zoology.ubc.ca>
2012-05-16 19:18:13 -0700

    Tests for the meristic mkn

--- diversitree-tests/runit-mkn.R
+++ diversitree-tests/runit-mkn.R
@@ -32,3 +32,114 @@ test.mkn <- function() {
 
   checkEquals(fit.ode[-7], fit.mkn[-7], tolerance=1e-7)
 }
+
+test.mkn.meristic <- function() {
+  library(diversitree)
+  library(RUnit)
+  ## Simulate a tree and character distribution.  This is on a birth-death
+  ## tree, with high rates of character evolution and an asymmetry in the
+  ## character transition rates.
+  set.seed(3)
+  phy <- tree.yule(.1, max.taxa=100)
+  k <- 31
+  r <- 3
+  states <- sim.character(phy, c(k, r, r), x0=(k+1)/2,
+                          model="meristic")
+
+  ## Constrain this:
+  p <- rep(0, k * (k-1))
+  tr <- expand.grid(from=seq_len(k), to=seq_len(k))
+  tr <- tr[tr$from != tr$to,]
+  base <- ceiling(log10(k + 0.5))
+  fmt <- sprintf("q%%0%dd%%0%dd", base, base)
+  par <- sprintf(fmt, tr$from, tr$to)
+  i.u <- tr$from - tr$to ==  1
+  i.d <- tr$from - tr$to == -1
+  i.o <- abs(tr$from - tr$to) > 1
+  u <- d <- r
+  p[i.d] <- d
+  p[i.u] <- u
+  
+  lik.mkn1 <- make.mkn(phy, states, k, strict=FALSE)
+  system.time(ll.mkn1 <- lik.mkn1(p))
+
+  lik.mkn2 <- make.mkn(phy, states, k, strict=FALSE,
+                       control=list(method="ode"))
+  system.time(ll.mkn2 <- lik.mkn2(p))
+
+  lik.mkn3 <- make.mkn(phy, states, k, strict=FALSE,
+                       control=list(method="ode", backend="cvodes"))
+  system.time(ll.mkn3 <- lik.mkn3(p))
+  
+  ## I have idea why this is *slower*, and not much faster than .3?
+  lik.mkn4 <- make.mkn(phy, states, k, strict=FALSE,
+                       control=list(method="ode", backend="CVODES"))
+  system.time(ll.mkn4 <- lik.mkn4(p))
+  
+  lik.mer1 <- make.mkn.meristic(phy, states, k)
+  system.time(ll.mer1 <- lik.mer1(c(r, r)))
+
+  lik.mer2 <- make.mkn.meristic(phy, states, k,
+                                control=list(backend="cvodes"))
+  system.time(ll.mer2 <- lik.mer2(c(d, u)))
+
+  ## This one is also not much faster.  Looks like LSODA just
+  ## slaughters CVODE here.  Probably need to tune the problem.
+  lik.mer3 <- make.mkn.meristic(phy, states, k,
+                                control=list(backend="CVODES"))
+  system.time(ll.mer3 <- lik.mer3(c(r, r)))
+
+  checkEquals(ll.mkn1, ll.mkn2, tol=5e-7)
+  checkEquals(ll.mkn1, ll.mkn3, tol=5e-7)
+  checkEquals(ll.mkn1, ll.mkn4, tol=5e-7)
+
+  checkEquals(ll.mkn1, ll.mer1, tol=5e-7)
+  checkEquals(ll.mkn1, ll.mer2, tol=5e-7)
+  checkEquals(ll.mkn1, ll.mer3, tol=5e-7)
+
+  ## ## ASYMMETRIC RATES
+  u <- 2 * r
+  p[i.d] <- d
+  p[i.u] <- u
+  
+  lik.mkn1 <- make.mkn(phy, states, k, strict=FALSE)
+  system.time(ll.mkn1 <- lik.mkn1(p))
+
+  lik.mkn2 <- make.mkn(phy, states, k, strict=FALSE,
+                       control=list(method="ode"))
+  system.time(ll.mkn2 <- lik.mkn2(p))
+
+  lik.mkn3 <- make.mkn(phy, states, k, strict=FALSE,
+                       control=list(method="ode", backend="cvodes"))
+  system.time(ll.mkn3 <- lik.mkn3(p))
+  
+  lik.mkn4 <- make.mkn(phy, states, k, strict=FALSE,
+                       control=list(method="ode", backend="CVODES"))
+  system.time(ll.mkn4 <- lik.mkn4(p))
+  
+  lik.mer1 <- make.mkn.meristic(phy, states, k)
+  system.time(ll.mer1 <- lik.mer1(c(d, u)))
+
+  lik.mer2 <- make.mkn.meristic(phy, states, k,
+                                control=list(backend="cvodes"))
+  system.time(ll.mer2 <- lik.mer2(c(d, u)))
+
+  ## This one is also not much faster.  Looks like LSODA just
+  ## slaughters CVODE here.  Probably need to tune the problem.
+  lik.mer3 <- make.mkn.meristic(phy, states, k,
+                                control=list(backend="CVODES"))
+  system.time(ll.mer3 <- lik.mer3(c(d, u)))
+
+  checkEquals(ll.mkn1, ll.mkn2, tol=5e-5)
+  checkEquals(ll.mkn2, ll.mkn3, tol=5e-5)
+  checkEquals(ll.mkn2, ll.mkn4, tol=5e-5)
+
+  checkEquals(ll.mkn2, ll.mer1, tol=5e-5)
+  checkEquals(ll.mkn2, ll.mer2, tol=5e-5)
+  checkEquals(ll.mkn2, ll.mer3, tol=5e-5)
+
+  q1 <- diversitree:::mkn.Q(p)
+  q2 <- diversitree:::mkn.meristic.Q(c(d, u), k)
+  checkIdentical(q1, q2)  
+}
+
