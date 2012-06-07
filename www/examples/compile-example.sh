0116e9bf055881a1aaaebb92554cf9f761411505
Rich FitzJohn <fitzjohn@zoology.ubc.ca>
2012-06-07 10:42:38 -0700

    Notes about improving examples

--- www/examples/compile-example.sh
+++ www/examples/compile-example.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+## TODO: This should add some templating; the end-of-file part should
+## be automatically added, perhaps.  A diversitree.footer() R function
+## with 'asis' or something.  The pandoc call should include the
+## google analytics fragment so I can see what people find
+## interesting.
+
 BASE=$1
 
 cd $BASE
