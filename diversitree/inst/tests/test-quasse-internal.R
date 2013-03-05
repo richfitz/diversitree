bf72b69f998e024ee9e3fa87a5766e810218ba2e
Rich FitzJohn <rich.fitzjohn@gmail.com>
2013-03-05 15:37:29 +1100

    Ported QuaSSE tests to testthat

--- diversitree/inst/tests/test-quasse-internal.R
+++ diversitree/inst/tests/test-quasse-internal.R
@@ -1,49 +1,41 @@
-## Tests of the QuaSSE internal functions.
-test.quasse.internal.nodrift <- function() {
-  dotest.quasse.internal(0)
-}
+library(diversitree)
+library(testthat)
 
-test.quasse.internal.drift <- function() {
-  dotest.quasse.internal(.01)
-}
+context("QuaSSE (internal)")
 
-dotest.quasse.internal <- function(drift=0) {
-  library(diversitree)
-  library(RUnit)
-  
-  
-  ## Imports that are generally hidden.
-  quasse.extent <- diversitree:::quasse.extent
-  expand.pars.quasse <- diversitree:::expand.pars.quasse
-  make.pde.quasse.fftC <- diversitree:::make.pde.quasse.fftC
-  make.pde.quasse.fftR <- diversitree:::make.pde.quasse.fftR
-  make.pde.quasse.mol <- diversitree:::make.pde.quasse.mol
+## Imports that are generally hidden.
+quasse.extent <- diversitree:::quasse.extent
+expand.pars.quasse <- diversitree:::expand.pars.quasse
+make.pde.quasse.fftC <- diversitree:::make.pde.quasse.fftC
+make.pde.quasse.fftR <- diversitree:::make.pde.quasse.fftR
+make.pde.quasse.mol <- diversitree:::make.pde.quasse.mol
 
-  make.branches.quasse.fftC <- diversitree:::make.branches.quasse.fftC
-  make.branches.quasse.fftR <- diversitree:::make.branches.quasse.fftR
-  make.branches.quasse.mol <- diversitree:::make.branches.quasse.mol
+make.branches.quasse.fftC <- diversitree:::make.branches.quasse.fftC
+make.branches.quasse.fftR <- diversitree:::make.branches.quasse.fftR
+make.branches.quasse.mol <- diversitree:::make.branches.quasse.mol
 
-  ## Basic control list.
-  control.fft <- list(tc=1.3,
-                      dt.max=1/20,
-                      nx=1024,
-                      dx=0.01,
-                      r=4L,
-                      xmid=0,
-                      w=5,
-                      flags=0L,
-                      verbose=0L,
-                      atol=1e-6,
-                      rtol=1e-6,
-                      eps=1e-3,
-                      method="fftC")
+## Basic control list.
+control.fft <- list(tc=1.3,
+                    dt.max=1/20,
+                    nx=1024,
+                    dx=0.01,
+                    r=4L,
+                    xmid=0,
+                    w=5,
+                    flags=0L,
+                    verbose=0L,
+                    atol=1e-6,
+                    rtol=1e-6,
+                    eps=1e-3,
+                    method="fftC")
 
-  lambda <- sigmoid.x
-  mu <- constant.x
-  diffusion <- 0.01
-  sd <- 1/20
-  len <- 2 # Integrate down a branch length of 2
+lambda <- sigmoid.x
+mu <- constant.x
+diffusion <- 0.01
+sd <- 1/20
+len <- 2 # Integrate down a branch length of 2
 
+for ( drift in c(0, .01) ) {
   args <- list(lambda=1:4, mu=5, drift=6, diffusion=7)
   pars <- c(.1, .2, 0, 2.5, .03, drift, diffusion)
 
@@ -61,8 +53,8 @@ dotest.quasse.internal <- function(drift=0) {
   vars.mol[seq_len(ndat),2] <- dnorm(ext.mol$x[[2]], 0, sd)
 
   ## TEST
-  checkEquals(vars.mol, vars.fft[seq_len(ndat),])
-  checkIdentical(vars.mol, vars.fft[seq_len(ndat),])
+  expect_that(vars.mol,          equals(vars.fft[seq_len(ndat),]))
+  expect_that(vars.mol, is_identical_to(vars.fft[seq_len(ndat),]))
 
   pars.fft <-
     expand.pars.quasse(lambda, mu, args, ext.fft, pars)
@@ -71,8 +63,8 @@ dotest.quasse.internal <- function(drift=0) {
 
   ## TEST
   ## after 5 is padding, which can be dropped (not relevant for MOL)
-  checkEquals(pars.fft$lo[1:5], pars.mol$lo[1:5])
-  checkIdentical(pars.fft$lo[1:5], pars.mol$lo[1:5])
+  expect_that(pars.fft$lo[1:5],          equals(pars.mol$lo[1:5]))
+  expect_that(pars.fft$lo[1:5], is_identical_to(pars.mol$lo[1:5]))
 
   pde.fftC <- with(control.fft, make.pde.quasse.fftC(nx, dx, dt.max, 2L, flags))
   pde.fftR <- with(control.fft, make.pde.quasse.fftR(nx, dx, dt.max, 2L))
@@ -82,14 +74,15 @@ dotest.quasse.internal <- function(drift=0) {
   ans.fftR <- pde.fftR(vars.fft, len, pars.fft$lo, 0)
 
   ## TEST
-  checkEquals(ans.fftC, ans.fftR)
+  expect_that(ans.fftC, equals(ans.fftR))
 
   if ( drift == 0 ) {
     ans.mol <- pde.mol(vars.mol, len, pars.mol$lo, 0)
-    checkEquals(ans.mol[[1]], ans.fftC[[1]], tolerance=1e-5)
-    checkEquals(ans.mol[[2]], ans.fftC[[2]][seq_len(ndat),], tolerance=0.0004)
+    expect_that(ans.mol[[1]], equals(ans.fftC[[1]], tolerance=1e-5))
+    expect_that(ans.mol[[2]], equals(ans.fftC[[2]][seq_len(ndat),],
+                                     tolerance=0.0004))
   }
-  
+
   branches.fftC <- make.branches.quasse.fftC(control.fft)
   branches.fftR <- make.branches.quasse.fftR(control.fft)
   branches.mol  <- make.branches.quasse.mol(control.mol)
@@ -106,14 +99,12 @@ dotest.quasse.internal <- function(drift=0) {
   ans.b.fftR <- branches.fftR(vars.hi.fft, len, pars.fft, 0)
 
   ## TEST:
-  checkEquals(ans.b.fftC, ans.b.fftR)
+  expect_that(ans.b.fftC, equals(ans.b.fftR))
 
   if ( drift == 0 ) {
     ans.b.mol <- branches.mol(vars.hi.mol, len, pars.mol, 0)
-    checkEquals(c(ans.b.fftC[1],
+    expect_that(c(ans.b.fftC[1],
                   matrix(ans.b.fftC[-1], ncol=2)[seq_len(ndat),]),
-                ans.b.mol, tolerance=0.00015)
+                equals(ans.b.mol, tolerance=0.00015))
   }
 }
-
-
